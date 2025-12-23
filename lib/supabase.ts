a58fb1e8132e3cec84da03a5950b9d4153870813
FreemanPT8 <freemanpt@proton.me>
2025-12-23 20:42:46 +0000

    fix: use public supabase entrypoint

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,5 @@
 // lib/supabase.ts
-import { createClient as createSupabaseClient } from '@supabase/supabase-js/dist/module/index.js';
+import { createClient as createSupabaseClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
