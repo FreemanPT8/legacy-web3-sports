e9ac9a6fb232d31d1488a451108dc6810a001e51
FreemanPT8 <freemanpt@proton.me>
2025-12-23 18:18:58 +0000

    fix: switch supabase client to ESM build

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js/dist/module/index.js';
 
 const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
